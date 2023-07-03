275056bcf93571e7189f1e0bdcb1231fc2e4398b
Andrey Fedorov <a.e.fedorov@inbox.ru>
2023-07-03 03:28:06 +0300

    Initial tests

--- src/test/java/ru/inno/market/MarketServiceTest.java
+++ src/test/java/ru/inno/market/MarketServiceTest.java
@@ -1,4 +1,47 @@
 package ru.inno.market;
 
+
+import org.junit.After;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.inno.market.core.MarketService;
+import ru.inno.market.model.Client;
+import ru.inno.market.model.Item;
+import ru.inno.market.model.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MarketServiceTest {
+    MarketService service;
+    Client Client1;
+
+    Item Item1 = new Item(8, "Microsoft Windows 11 Pro", Category.SOFTWARE, 5200);
+    @BeforeEach
+    public void setUp() {
+        service = new MarketService();
+    }
+
+    @Test
+    @DisplayName("Создание заказа")
+    public void CreateOrderFor() {
+        int Id = service.createOrderFor(Client1);
+        assertEquals(0, Id);
+    }
+
+    @Test
+    @DisplayName("Добавление товара к заказу")
+    public void AddItemToOrder() {
+        int newId = service.createOrderFor(Client1);
+        service.addItemToOrder(Item1,newId);
+        assertEquals(5200, service.getOrderInfo(newId).getTotalPrice());
+    }
+
+    @After
+    public  void Delete(){
+        service = null;
+        Item1 = null;
+        Client1 = null;
+    }
+
 }
--- src/test/java/ru/inno/market/OrderTest.java
+++ src/test/java/ru/inno/market/OrderTest.java
@@ -1,4 +1,51 @@
 package ru.inno.market;
 
+import org.junit.After;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.inno.market.model.Category;
+import ru.inno.market.model.Client;
+import ru.inno.market.model.Item;
+import ru.inno.market.model.Order;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class OrderTest {
+     Client Client1 = new Client(1, "Иван Иванович Иванов");
+     Item Item1 = new Item(8, "Microsoft Windows 11 Pro", Category.SOFTWARE, 5200);
+
+    @Test
+    @DisplayName("Создание заказа с корректной информацией")
+    public void CreateOrder () {
+        Order order1 = new Order(1, Client1);
+        assertEquals(1, order1.getId());
+    }
+
+    @Test
+    @DisplayName("Создание заказа с ошибкой")
+    public void CreateOrderWithBug () {
+        Order order1 = new Order(1, Client1);
+        assertEquals(5, order1.getId());
+    }
+
+    @Test
+    @DisplayName("Добавление товара в корзину")
+    public void AddItemPositive() {
+        Order order = new Order(1, Client1);
+        order.addItem(Item1);
+        assertEquals(5200, order.getTotalPrice());
+    }
+
+    @Test
+    @DisplayName("Добавление товара в корзину с неправильным Id")
+    public void AddItemNegative() {
+        Order order = new Order(3, Client1);
+        order.addItem(Item1);
+        assertEquals(1, order.getId());
+    }
+    @After
+    public  void shouldDelete(){
+        Item1 = null;
+        Client1 = null;
+    }
 }
